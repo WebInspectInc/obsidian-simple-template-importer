c07946e7ee8e79d21258811b1862127d0d91e17f
Tim Miller <tim@webinspect.tech>
2025-06-03 10:21:39 -0500

    Add ability to import zip files into vault

--- main.ts
+++ main.ts
@@ -1,134 +1,113 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Plugin, PluginSettingTab, Setting, TFile, TFolder, Notice } from 'obsidian';
+import JSZip from 'jszip';
+import * as path from 'path';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface VaultImporterSettings {
+    importPath: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: VaultImporterSettings = {
+    importPath: ''
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class VaultImporterPlugin extends Plugin {
+    settings: VaultImporterSettings;
 
-	async onload() {
-		await this.loadSettings();
+    async onload() {
+        await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+        // Add command to import zip file
+        this.addCommand({
+            id: 'import-zip-vault',
+            name: 'Import Files from ZIP',
+            callback: () => this.importZipFile()
+        });
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+        // Add settings tab
+        this.addSettingTab(new VaultImporterSettingTab(this.app, this));
+    }
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+    async loadSettings() {
+        this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+    }
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+    async saveSettings() {
+        await this.saveData(this.settings);
+    }
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+    async importZipFile() {
+        // Create file input element
+        const input = document.createElement('input');
+        input.type = 'file';
+        input.accept = '.zip';
+        
+        input.onchange = async (e) => {
+            const file = (e.target as HTMLInputElement).files?.[0];
+            if (!file) return;
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+            try {
+                const zip = new JSZip();
+                const zipContent = await zip.loadAsync(file);
+                
+                // Process each file in the zip
+                for (const [filePath, zipEntry] of Object.entries(zipContent.files)) {
+                    if (zipEntry.dir) continue; // Skip directories
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
+                    // Get file content
+                    const content = await (zipEntry as JSZip.JSZipObject).async('string');
+                    
+                    // Create the file in the vault
+                    const targetPath = path.join(this.settings.importPath, filePath);
+                    
+                    // Ensure the directory exists
+                    const targetDir = path.dirname(targetPath);
+                    if (targetDir !== '.') {
+                        try {
+                            await this.app.vault.createFolder(targetDir);
+                        } catch (error) {
+                            // Ignore error if folder already exists
+                            if (!error.message.includes('already exists')) {
+                                throw error;
+                            }
+                        }
+                    }
+                    
+                    await this.app.vault.create(targetPath, content);
+                }
 
-	onunload() {
+                new Notice('Files imported successfully!');
+            } catch (error) {
+                new Notice(`Error importing files: ${error.message}`);
+                console.error('Import error:', error);
+            }
+        };
 
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+        input.click();
+    }
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class VaultImporterSettingTab extends PluginSettingTab {
+    plugin: VaultImporterPlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+    constructor(app: App, plugin: VaultImporterPlugin) {
+        super(app, plugin);
+        this.plugin = plugin;
+    }
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+    display(): void {
+        const {containerEl} = this;
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+        containerEl.empty();
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
-}
+        new Setting(containerEl)
+            .setName('Import Path')
+            .setDesc('The folder where imported files will be placed (leave blank for root folder)')
+            .addText(text => text
+                .setPlaceholder('Enter import path')
+                .setValue(this.plugin.settings.importPath)
+                .onChange(async (value) => {
+                    this.plugin.settings.importPath = value;
+                    await this.plugin.saveSettings();
+                }));
+    }
+} 
