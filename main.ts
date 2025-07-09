c3159bbf07a9e819cbfa9ada56ff62f61aa67b72
Tim Miller <tim@webinspect.tech>
2025-07-09 06:39:24 -0500

    Fix change requests
    Also reformats ts file and bumps version

--- main.ts
+++ main.ts
@@ -2,111 +2,111 @@ import { App, Plugin, PluginSettingTab, Setting, TFile, TFolder, Notice } from '
 import JSZip from 'jszip';
 
 interface VaultImporterSettings {
-    importPath: string;
+	importPath: string;
 }
 
 const DEFAULT_SETTINGS: VaultImporterSettings = {
-    importPath: ''
+	importPath: ''
 }
 
 export default class VaultImporterPlugin extends Plugin {
-    settings: VaultImporterSettings;
+	settings: VaultImporterSettings;
 
-    async onload() {
-        await this.loadSettings();
+	async onload() {
+		await this.loadSettings();
 
-        // Add command to import zip file
-        this.addCommand({
-            id: 'import-zip-vault',
-            name: 'Import Files from ZIP',
-            callback: () => this.importZipFile()
-        });
+		// Add command to import zip file
+		this.addCommand({
+			id: 'import-zip-vault',
+			name: 'Import files from ZIP',
+			callback: () => this.importZipFile()
+		});
 
-        // Add settings tab
-        this.addSettingTab(new VaultImporterSettingTab(this.app, this));
-    }
+		// Add settings tab
+		this.addSettingTab(new VaultImporterSettingTab(this.app, this));
+	}
 
-    async loadSettings() {
-        this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-    }
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
 
-    async saveSettings() {
-        await this.saveData(this.settings);
-    }
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
 
-    async importZipFile() {
-        // Create file input element
-        const input = document.createElement('input');
-        input.type = 'file';
-        input.accept = '.zip';
-        
-        input.onchange = async (e) => {
-            const file = (e.target as HTMLInputElement).files?.[0];
-            if (!file) return;
+	async importZipFile() {
+		// Create file input element
+		const input = document.createElement('input');
+		input.type = 'file';
+		input.accept = '.zip';
 
-            try {
-                const zip = new JSZip();
-                const zipContent = await zip.loadAsync(file);
+		input.onchange = async (e) => {
+			const file = (e.target as HTMLInputElement).files?.[0];
+			if (!file) return;
+
+			try {
+				const zip = new JSZip();
+				const zipContent = await zip.loadAsync(file);
 				const imageTypes = ['.png', '.jpg', '.jpeg', '.gif'];
 				const overwriteFiles = this.settings.overwriteFiles;
 
-                // Process each file in the zip
-                for (const [filePath, zipEntry] of Object.entries(zipContent.files)) {
-                    if (zipEntry.dir) continue; // Skip directories
+				// Process each file in the zip
+				for (const [filePath, zipEntry] of Object.entries(zipContent.files)) {
+					if (zipEntry.dir) continue; // Skip directories
 					if (zipEntry.name.indexOf('/.') !== -1) continue; // skip hidden files
 
 					const fileName = filePath.replace(/^.*[\\/]/, '');
 					let content;
 
-                    // Get file content
+					// Get file content
 					if (imageTypes.some(e => fileName.endsWith(e))) {
 						content = await (zipEntry as JSZip.JSZipObject).async('arrayBuffer');
 					} else {
 						content = await (zipEntry as JSZip.JSZipObject).async('string');
 					}
-                    
-                    let targetPath = this.settings.importPath + "/" + filePath;
 
-                    // Handle CSS files specially
+					let targetPath = this.settings.importPath + "/" + filePath;
+
+					// Handle CSS files specially
 					// If there are css files in the template, we move them to the snippets folder
-                    if (filePath.endsWith('.css')) {
-                        try {
-                            const snippetsPath = this.app.vault.configDir + '/snippets';
+					if (filePath.endsWith('.css')) {
+						try {
+							const snippetsPath = this.app.vault.configDir + '/snippets';
 							try {
 								await this.app.vault.createFolder(snippetsPath)
 							} catch { /* Snippets folder already exists */ }
-                            
-                            // Move CSS file to snippets folder
-                            targetPath = snippetsPath + "/" + fileName;
-                            try {
-                                await this.app.vault.create(targetPath, content);
-                            } catch (error) {
+
+							// Move CSS file to snippets folder
+							targetPath = snippetsPath + "/" + fileName;
+							try {
+								await this.app.vault.create(targetPath, content);
+							} catch (error) {
 								if (overwriteFiles) await this.app.vault.adapter.write(targetPath, content);
 							}
 
-                            continue;
-                        } catch (error) {
+							continue;
+						} catch (error) {
 							// error handling css file, it will stay in the template folder instead
-                        }
-                    }
-                    
-                    // Ensure file directories exist
-                    const targetDir = targetPath.replace(fileName, '');
+						}
+					}
+
+					// Ensure file directories exist
+					const targetDir = targetPath.replace(fileName, '');
 					console.log(targetDir);
-                    if (targetDir !== '.') {
-                        try {
-                            await this.app.vault.createFolder(targetDir);
-                        } catch (error) {
-                            // Ignore folder if it already exists
-                            if (!error.message.includes('already exists')) continue; 
-                        }
-                    }
-					
+					if (targetDir !== '.') {
+						try {
+							await this.app.vault.createFolder(targetDir);
+						} catch (error) {
+							// Ignore folder if it already exists
+							if (!error.message.includes('already exists')) continue;
+						}
+					}
+
 					// Handle images
 					if (imageTypes.some(e => fileName.endsWith(e))) {
 						try {
 							await this.app.vault.createBinary(targetPath, content);
-							new Notice (`Image created: ${fileName}`);
+							new Notice(`Image created: ${fileName}`);
 						} catch (error) {
 							if (overwriteFiles) {
 								new Notice(`Image updated: ${fileName}`);
@@ -117,8 +117,8 @@ export default class VaultImporterPlugin extends Plugin {
 						}
 						continue;
 					}
-                    
-                    // Handle all other file in the vault
+
+					// Handle all other file in the vault
 					try {
 						await this.app.vault.create(targetPath, content);
 						new Notice(`File created: ${fileName}`);
@@ -131,45 +131,45 @@ export default class VaultImporterPlugin extends Plugin {
 						}
 						console.log(error);
 					}
-                }
+				}
 
-                new Notice('Files imported successfully!');
-            } catch (error) {
-                new Notice(`Error importing files: ${error.message}`);
-                console.warn('Import error:', error);
-            }
-        };
+				new Notice('Files imported successfully!');
+			} catch (error) {
+				new Notice(`Error importing files: ${error.message}`);
+				console.warn('Import error:', error);
+			}
+		};
 
-        input.click();
-    }
+		input.click();
+	}
 }
 
 class VaultImporterSettingTab extends PluginSettingTab {
-    plugin: VaultImporterPlugin;
+	plugin: VaultImporterPlugin;
 
-    constructor(app: App, plugin: VaultImporterPlugin) {
-        super(app, plugin);
-        this.plugin = plugin;
-    }
+	constructor(app: App, plugin: VaultImporterPlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
 
-    display(): void {
-        const {containerEl} = this;
+	display(): void {
+		const { containerEl } = this;
 
-        containerEl.empty();
-
-        new Setting(containerEl)
-            .setName('Import Path')
-            .setDesc('The folder where imported files will be placed (leave blank for root folder)')
-            .addText(text => text
-                .setPlaceholder('Enter import path')
-                .setValue(this.plugin.settings.importPath)
-                .onChange(async (value) => {
-                    this.plugin.settings.importPath = value;
-                    await this.plugin.saveSettings();
-                }));
+		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Overwrite Files')
+			.setName('Import path')
+			.setDesc('The folder where imported files will be placed (leave blank for root folder)')
+			.addText(text => text
+				.setPlaceholder('Enter import path')
+				.setValue(this.plugin.settings.importPath)
+				.onChange(async (value) => {
+					this.plugin.settings.importPath = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Overwrite files')
 			.setDesc('Be careful! This setting will cause existing templates to be overwritten. This is desirable if you are updating a template, but otherwise I would leave this off.')
 			.addToggle(text => text
 				.setValue(this.plugin.settings.overwriteFiles)
@@ -178,5 +178,5 @@ class VaultImporterSettingTab extends PluginSettingTab {
 					await this.plugin.saveSettings();
 				})
 			);
-    }
-} 
+	}
+}
